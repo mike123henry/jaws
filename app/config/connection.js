dcf70364c71def1c168ad800cfc9fbf6ce217978
MikeH <mike123.henry@gmail.com>
2016-07-26 19:26:51 -0500

    connection mods with heroku jawsDB

--- app/config/connection.js
+++ app/config/connection.js
@@ -15,15 +15,18 @@ var source = {
         database: "starwars"
     },
     jawsDB: {
-
+        Host: 'l9dwvv6j64hlhpul.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        Username: 'vo7g2cg25tu0lk5j',
+        Password: 'd0hk2hgna2q7p6rp',
+        Port: 3306,
+        database: 'q0orx3zic7scjy0p'
         // TODO: Add your JawsDB connection info here
     }
 }
 
 
 // TODO: connect your jawsDB connection witht he mysql connection.
-var connection = mysql.createConnection( //enter connection here);
-
+var connection = mysql.createConnection(source.jawsDB)
 
 
 // Replace the connection code here with one to connect to you JawsDB connection.
